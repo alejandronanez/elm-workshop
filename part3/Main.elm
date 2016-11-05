215ad3a362c00ba3c1b3050351e98dce8437d70e
Alejandro Nanez <alejonanez@gmail.com>
2016-11-05 17:29:49 -0500

    Filtering results

--- part3/Main.elm
+++ part3/Main.elm
@@ -54,15 +54,21 @@ viewSearchResult result =
             [ text result.name ]
         , button
             -- TODO add an onClick handler that sends a DELETE_BY_ID msg
-            [ class "hide-result" ]
+            [ class "hide-result"
+            , onClick { operation = "DELETE_BY_ID", data = result.id }
+            ]
             [ text "X" ]
         ]
 
 
 update msg model =
-    -- TODO if msg.operation == "DELETE_BY_ID",
-    -- then return a new model without the given ID present anymore.
-    model
+    if msg.operation == "DELETE_BY_ID" then
+        { model | results = List.filter (\result -> result.id /= msg.data) model.results }
+        -- { model | results = List.filter deleteMsg model.results }
+    else
+        -- TODO if msg.operation == "DELETE_BY_ID",
+        -- then return a new model without the given ID present anymore.
+        model
 
 
 main =

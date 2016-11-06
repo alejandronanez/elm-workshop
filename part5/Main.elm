8b98233b407e461f0876a3d561facc35f5e1c0d0
Alejandro Nanez <alejonanez@gmail.com>
2016-11-06 00:13:49 -0500

    Query update

--- part5/Main.elm
+++ part5/Main.elm
@@ -69,6 +69,7 @@ view model =
             ]
         , input
             [ class "search-query"
+            , onInput SetQuery
               -- TODO onInput, set the query in the model
             , defaultValue model.query
             ]
@@ -85,17 +86,26 @@ viewSearchResult result =
         , a [ href ("https://github.com/" ++ result.name), target "_blank" ]
             [ text result.name ]
         , button
-            -- TODO add an onClick handler that sends a DeleteById msg
-            [ class "hide-result" ]
+            [ class "hide-result"
+            , onClick (DeleteById result.id)
+            ]
             [ text "X" ]
         ]
 
 
 update : Msg -> Model -> Model
 update msg model =
-    -- TODO if we get a SetQuery msg, use it to set the model's query field,
-    -- and if we get a DeleteById msg, delete the appropriate result
-    model
+    case msg of
+        SetQuery query ->
+            { model | query = query }
+
+        DeleteById id ->
+            { model | results = List.filter (\result -> result.id /= id) model.results }
+
+
+
+-- TODO if we get a SetQuery msg, use it to set the model's query field,
+-- and if we get a DeleteById msg, delete the appropriate result
 
 
 main : Program Never

6aa0787da9b8e93da6eeab8bb431b06eda8797a1
Alejandro Nanez <alejonanez@gmail.com>
2016-11-05 20:14:38 -0500

    Type annotations added

--- part4/Main.elm
+++ part4/Main.elm
@@ -29,6 +29,7 @@ type alias Msg =
 
 HINT: The type aliases above may come in handy for these exercises!
 -}
+initialModel : Model
 initialModel =
     { query = "tutorial"
     , results =
@@ -56,8 +57,7 @@ initialModel =
     }
 
 
-{-| TODO add a type annotation to this value
--}
+elmHubHeader : Html Msg
 elmHubHeader =
     header []
         [ h1 [] [ text "ElmHub" ]
@@ -65,8 +65,7 @@ elmHubHeader =
         ]
 
 
-{-| TODO add a type annotation to this function
--}
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ elmHubHeader
@@ -76,6 +75,7 @@ view model =
 
 {-| TODO add a type annotation to this function
 -}
+viewSearchResult : SearchResult -> Html Msg
 viewSearchResult result =
     li []
         [ span [ class "star-count" ] [ text (toString result.stars) ]
@@ -87,8 +87,7 @@ viewSearchResult result =
         ]
 
 
-{-| TODO add a type annotation to this function
--}
+update : Msg -> Model -> Model
 update msg model =
     if msg.operation == "DELETE_BY_ID" then
         { model
@@ -98,6 +97,7 @@ update msg model =
         model
 
 
+main : Program Never
 main =
     Html.beginnerProgram
         { view = view
